1e93b9f6a336adb35f41894a6f67cde17434508e
richardktran <richardktran.dev@gmail.com>
2023-07-08 00:24:42 +0700

    setup docker

---
+++ app/Http/Controllers/ContentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ContentController extends Controller
+{
+    public function index()
+    {
+        return ([
+            'content-1' => 'This is content 1',
+            'content-2' => 'This is content 2',
+            'content-3' => 'This is content 3',
+            'content-4' => 'This is content 4',
+            'content-5' => 'This is content 5',
+        ]);
+    }
+}
